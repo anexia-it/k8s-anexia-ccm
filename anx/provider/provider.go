0bff637fd6fe87af9890034f6278c65ff4ef0bd5
Mara Sophie Grosch <mgrosch@anexia-it.com>
2022-02-18 13:10:21 +0100

    💩✨🐛: store correct LoadBalancer IP in service
    This determines the correct external LoadBalancer IP from the discovered
    prefixes, doing some black magic IP calculation. The code is prepared to
    allocate new addresses and even prefixes, with lots of comments which
    code is MVP-only and has to be nuked later (like the black magic IP
    calculation).
    
    Also checks if another Service in the cluster already uses the one
    usable LoadBalancer IP and returns an error, which is added as event to
    the Service, making it visible for the customer.

--- anx/provider/provider.go
+++ anx/provider/provider.go
@@ -13,6 +13,7 @@ import (
 
 	anexia "go.anx.io/go-anxcloud/pkg"
 	anxClient "go.anx.io/go-anxcloud/pkg/client"
+
 	cloudprovider "k8s.io/cloud-provider"
 	"k8s.io/klog/v2"
 )
@@ -90,7 +91,7 @@ func (a *anxProvider) Initialize(builder cloudprovider.ControllerClientBuilder, 
 		}
 	}
 
-	a.loadBalancerManager = newLoadBalancerManager(a)
+	a.loadBalancerManager = newLoadBalancerManager(a, builder)
 
 	if a.isLBaaSReplicationEnabled() {
 		a.loadBalancerManager.notify = make(chan struct{}, 1)
