174f23a08dbbd9c77aaecec3f91a0bb1e319a0e1
Mario Schäfer <mschaefer@anexia-it.com>
2023-01-18 11:34:56 +0100

    Panic on 401 responses from engine to slow down requests with invalid token

--- anx/provider/utils/utils.go
+++ anx/provider/utils/utils.go
@@ -2,8 +2,10 @@ package utils
 
 import (
 	"errors"
-	"go.anx.io/go-anxcloud/pkg/client"
 	"net/http"
+
+	"go.anx.io/go-anxcloud/pkg/api"
+	"go.anx.io/go-anxcloud/pkg/client"
 )
 
 func IsNotFoundError(err error) bool {
@@ -18,4 +20,23 @@ func IsNotFoundError(err error) bool {
 		}
 	}
 	return false
+}
+
+// PanicIfUnauthorized panics if the provided error is due to `http.StatusUnauthorized`.
+// Use this helper to slow down Engine requests by triggering a CrashLoopBackoff.
+// NOTE: This helper only works for errors returned by go-anxcloud.
+func PanicIfUnauthorized(err error) {
+	if err == nil {
+		return
+	}
+
+	var (
+		genericAPIClientError api.HTTPError
+		legacyAPIClientError  *client.ResponseError
+	)
+
+	if (errors.As(err, &genericAPIClientError) && genericAPIClientError.StatusCode() == http.StatusUnauthorized) ||
+		(errors.As(err, &legacyAPIClientError) && legacyAPIClientError.Response.StatusCode == http.StatusUnauthorized) {
+		panic("Engine responded with http.StatusUnauthorized. Please check the configured API token.")
+	}
 }
