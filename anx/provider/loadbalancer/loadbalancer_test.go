53896c9519893606c5e5ee5d588a1ce46874fca4
Manuel Zangl <mzangl@anexia-it.com>
2023-03-06 16:47:19 +0100

    Update test dependencies
    Add basic Ginko test for loadbalancer
    Move provider test to Ginko

--- anx/provider/loadbalancer/loadbalancer_test.go
+++ anx/provider/loadbalancer/loadbalancer_test.go
@@ -3,9 +3,38 @@ package loadbalancer
 import (
 	"testing"
 
+	"github.com/anexia-it/k8s-anexia-ccm/anx/provider/configuration"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"go.anx.io/go-anxcloud/pkg/api"
+	"go.anx.io/go-anxcloud/pkg/client"
+	"k8s.io/klog/v2/klogr"
 )
+
+var _ = Describe("Initialization", func() {
+	It("should initialize loadbalancer", func() {
+		config := configuration.ProviderConfig{
+			Token:      "RANDOM_VALUE",
+			CustomerID: "CUSTOMER",
+		}
+
+		logger := klogr.NewWithOptions()
+
+		legacyClient, _ := client.New(client.TokenFromString(config.Token))
+
+		genericClient, _ := api.NewAPI(
+			api.WithClientOptions(
+				client.TokenFromString(config.Token),
+			),
+			api.WithLogger(logger.WithName("go-anxcloud")),
+		)
+
+		loadbalancer, err := New(&config, logger, nil, genericClient, legacyClient)
+
+		Expect(loadbalancer).ToNot(BeNil())
+		Expect(err).Error().ToNot(HaveOccurred())
+	})
+})
 
 func TestLoadBalancer(t *testing.T) {
 	RegisterFailHandler(Fail)
--- anx/provider/provider_test.go
+++ anx/provider/provider_test.go
@@ -8,48 +8,53 @@ import (
 
 	"github.com/anexia-it/k8s-anexia-ccm/anx/provider/configuration"
 	"github.com/anexia-it/k8s-anexia-ccm/anx/provider/metrics"
-	"github.com/stretchr/testify/require"
 	cloudprovider "k8s.io/cloud-provider"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-func TestNewProvider(t *testing.T) {
-	provider, err := newAnxProvider(configuration.ProviderConfig{
-		Token:      "RANDOME_VALUE",
-		CustomerID: "CUSTOMER",
+var _ = Describe("Initialization", func() {
+	It("should initialize a new provider", func() {
+		provider, err := newAnxProvider(configuration.ProviderConfig{
+			Token:      "RANDOME_VALUE",
+			CustomerID: "CUSTOMER",
+		})
+		Expect(err).Error().ToNot(HaveOccurred())
+		Expect(provider).ToNot(BeNil())
+
+		provider.Initialize(nil, nil)
+		instances, instancesEnabled := provider.Instances()
+		instancesV2, instancesV2Enabled := provider.InstancesV2()
+		zones, zonesEnabled := provider.Zones()
+		hasClusterID := provider.HasClusterID()
+		routes, routesEnabled := provider.Routes()
+		clusters, clustersEnabled := provider.Clusters()
+		providerName := provider.ProviderName()
+		loadbalancer, loadbalancerEnabled := provider.LoadBalancer()
+
+		Expect(instances).To(BeNil())
+		Expect(instancesV2).ToNot(BeNil())
+		Expect(provider.providerMetrics).ToNot(BeNil())
+		Expect(zones).To(BeNil())
+		Expect(routes).To(BeNil())
+		Expect(clusters).To(BeNil())
+		Expect(loadbalancer).ToNot(BeNil())
+		Expect(instancesEnabled).To(BeFalse())
+		Expect(instancesV2Enabled).To(BeTrue())
+		Expect(zonesEnabled).To(BeFalse())
+		Expect(routesEnabled).To(BeFalse())
+		Expect(hasClusterID).To(BeTrue())
+		Expect(clustersEnabled).To(BeFalse())
+		Expect(loadbalancerEnabled).To(BeTrue())
+
+		Expect(configuration.CloudProviderName).To(Equal(providerName))
+		Expect(instanceManager{}).To(BeAssignableToTypeOf(instancesV2))
+		manager := instancesV2.(instanceManager)
+		Expect(provider).To(Equal(manager.Provider))
 	})
-	require.NoError(t, err)
-	require.NotNil(t, provider)
 
-	provider.Initialize(nil, nil)
-	instances, instancesEnabled := provider.Instances()
-	instancesV2, instancesV2Enabled := provider.InstancesV2()
-	zones, zonesEnabled := provider.Zones()
-	hasClusterID := provider.HasClusterID()
-	routes, routesEnabled := provider.Routes()
-	clusters, clustersEnabled := provider.Clusters()
-	providerName := provider.ProviderName()
-	loadbalancer, loadbalancerEnabled := provider.LoadBalancer()
-
-	require.Nil(t, instances)
-	require.NotNil(t, instancesV2)
-	require.Nil(t, zones)
-	require.Nil(t, routes)
-	require.Nil(t, clusters)
-	require.NotNil(t, loadbalancer)
-	require.False(t, instancesEnabled)
-	require.True(t, instancesV2Enabled)
-	require.False(t, zonesEnabled)
-	require.False(t, routesEnabled)
-	require.True(t, hasClusterID)
-	require.False(t, clustersEnabled)
-	require.True(t, loadbalancerEnabled)
-
-	require.Equal(t, configuration.CloudProviderName, providerName)
-	require.IsType(t, instanceManager{}, instancesV2)
-	manager := instancesV2.(instanceManager)
-	require.Equal(t, provider, manager.Provider)
-
-	t.Run("with incomplete initialization", func(t *testing.T) {
+	It("should work with incomplete initialization", func() {
 		p := &anxProvider{
 			// we initialize `providerMetrics` manually because
 			// `anxProvider.setupProviderMetrics()` panics when called a second time
@@ -57,45 +62,55 @@ func TestNewProvider(t *testing.T) {
 		}
 
 		loadbalancer, loadbalancerEnabled := p.LoadBalancer()
-		require.False(t, loadbalancerEnabled)
-		require.Nil(t, loadbalancer)
+		Expect(loadbalancerEnabled).To(BeFalse())
+		Expect(loadbalancer).To(BeNil())
 
 		instancesV2, instancesV2Enabled := p.InstancesV2()
-		require.False(t, instancesV2Enabled)
-		require.Nil(t, instancesV2)
+		Expect(instancesV2Enabled).To(BeFalse())
+		Expect(instancesV2).To(BeNil())
 	})
 
-}
+	Context("Register cloud provider", func() {
+		BeforeEach(func() {
+			Expect(os.Setenv("ANEXIA_TOKEN", "TOKEN")).Error().ToNot(HaveOccurred())
+		})
 
-func TestRegisterCloudProvider(t *testing.T) {
-	require.NoError(t, os.Setenv("ANEXIA_TOKEN", "TOKEN"))
-	t.Cleanup(func() {
-		require.NoError(t, os.Unsetenv("ANEXIA_TOKEN"))
+		AfterEach(func() {
+			Expect(os.Unsetenv("ANEXIA_TOKEN")).Error().ToNot(HaveOccurred())
+		})
+
+		It("should register cloud provider", func() {
+			provider, err := cloudprovider.GetCloudProvider("anexia", bytes.NewReader([]byte("customerID: 555")))
+			Expect(err).Error().ToNot(HaveOccurred())
+			Expect(provider).ToNot(BeNil())
+
+			anxProvider, ok := provider.(*anxProvider)
+			Expect(ok).To(BeTrue())
+			config := anxProvider.Config()
+			Expect(config.CustomerID).To(Equal("555"))
+			Expect(config.Token).To(Equal("TOKEN"))
+		})
 	})
-	provider, err := cloudprovider.GetCloudProvider("anexia", bytes.NewReader([]byte("customerID: 555")))
-	require.NoError(t, err)
-	require.NotNil(t, provider)
-	anxProvider, ok := provider.(*anxProvider)
-	require.True(t, ok)
-	config := anxProvider.Config()
-	require.Equal(t, "555", config.CustomerID)
-	require.Equal(t, "TOKEN", config.Token)
-}
+})
 
-func TestProviderScheme(t *testing.T) {
-	t.Parallel()
-	require.Equal(t, fmt.Sprintf("%s://", configuration.CloudProviderName), configuration.CloudProviderScheme)
-}
-
-func TestProviderConfig(t *testing.T) {
-	t.Parallel()
-	provider, err := newAnxProvider(configuration.ProviderConfig{
-		Token:      "5555",
-		CustomerID: "5555",
+var _ = Describe("Config", func() {
+	It("should test provider schema", func() {
+		Expect(fmt.Sprintf("%s://", configuration.CloudProviderName)).To(Equal(configuration.CloudProviderScheme))
 	})
-	require.NoError(t, err)
 
-	config := provider.Config()
+	It("should test provider config", func() {
+		provider, err := newAnxProvider(configuration.ProviderConfig{
+			Token:      "5555",
+			CustomerID: "5555",
+		})
+		Expect(err).Error().ToNot(HaveOccurred())
 
-	require.Equal(t, "5555", config.CustomerID)
+		config := provider.Config()
+		Expect(config.CustomerID).To(Equal("5555"))
+	})
+})
+
+func TestProviderSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Provider Suite")
 }
